db36a25764c6abce73f927905a7e7b10ae1df730
xzf <270035679@qq.com>
2017-06-15 19:28:34 +0800

    change config

---
+++ sendPostRequest.js
@@ -0,0 +1,21 @@
+var http = require('http');
+
+var querystring = require('querystring');
+
+var options = {
+	host:'127.0.0.1',
+ 	port:9999,
+	path:'/refresh',
+	method:'POST'   
+};
+
+var req = http.request(options,function(res){
+	console.log('STATUS:'+res.statusCode);
+	console.log('HEADERS:'+JSON.stringify(res.headers));
+	res.setEncoding('utf8');
+	res.on('data',function(chunck){
+		console.log('BODY:'+chunck);
+	});
+});
+
+req.end();
